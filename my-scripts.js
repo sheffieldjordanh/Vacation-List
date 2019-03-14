1dc36868443638596d38255053d1a5113faea09d
sheffieldjordanh <48527220+sheffieldjordanh@users.noreply.github.com>
2019-03-13 21:13:11 -0600

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,25 @@
+//place to store data
+var myList = (JSON.parse(localStorage.getItem("myData")) || [])
+
+//create items
+function createItem() {
+	var newItem = {text:window.prompt("Add a vacation"), style:"none"}
+	myList.push(newItem)
+	localStorage.setItem("myData", JSON.stringify(myList))
+	printItem(newItem)
+}
+
+//print items
+function printItem(item){
+	var p =document.createElement("p")
+	p.innerHTML = item.text
+	p.className = item.style
+	p.onclick = function(){
+		this.className = item.style = (this.className=="none")? "stike" : "invisible"
+		localStorage.setItem("myData", JSON.stringify(myList))
+	}
+	document.getElementById("myDiv").appendChild(p)
+}
+
+//loop and print list
+for (x=0; x<myList.length;x++){printItem(myList[x])}
